3995a1a28d384e3a25f4eb1f2f3b340a5edc64e0
vahallla <89891803+vahallla@users.noreply.github.com>
2023-09-04 15:57:55 +0900

    html완성

---
+++ www/src/component/footer.js
@@ -0,0 +1,98 @@
+import React from 'react';
+
+const Footer = () => (
+    <div className="footer">
+        <section class="partner">
+        <p class="title">Family site</p>
+        <div class="LayoutStyles__FooterPartnerContainer-sc-iy7y4f-5 hOIhoX">
+            <div class="flicking-viewport" style="user-select: none; -webkit-user-drag: none; touch-action: pan-y;">
+                <div class="flicking-camera" style="transform: translate(-2640px);">
+                    <div class="panel">
+                        <img src="family/sponsor_1679274627498_자산 5.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1679274396424_진안군.jpg"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1679274347161_정읍시.jpg"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1679274313707_전주시.jpg"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1671523694133_20221220_165958.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1671523670270_20221220_165948.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1671523619775_20221220_165937.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1671523588474_완주군.jpg"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1671523563085_20221220_165904.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1671523543870_20221220_165853.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1671523466428_20221220_165840.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1671523430385_20221220_165829.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1671523352139_20221220_165819.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1671523321595_20221220_165809.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1671523297965_20221220_170110.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1670839890616_link4.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1670839867748_link3.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1670839850467_link2.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1669010656616_link1.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1679274956135_스카이항공 로고.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1679274896503_대지 1.png"></img>
+                    </div>
+                    <div class="panel">
+                        <img src="family/sponsor_1679274783853_자산 1.png"></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="footer-container">
+        <div class="logo-container">
+            <img class="logo" src="family/logo.png" alt="jbtour">
+            <div class="footer-contents-container">
+                <p>전라북도 전주시 덕진구 정언신로 95 경희프라자건물 7층</p>
+                <span>TEL.&nbsp;063-287-6292</span><span>FAX.&nbsp;063-288-6294</span>
+                <span style="word-break:keep-all">대표이사<!-- -->:&nbsp;조오익</span><br>
+                <span style="word-break:keep-all">사업자등록번호<!-- -->:&nbsp;<!-- -->418-82-04300 </span>
+                <span style="word-break:keep-all">통신판매업신고번호<!-- -->:&nbsp;<!-- -->제2017-전주덕진-0234호 </span><br>
+                <p>COPTRIGHT &nbsp;&nbsp; Jeollabuk-do Tourism Assiciation RIGHT RESERVED</p>
+            </div>
+        </div>
+        <div class="footer-about-container"><h4>전화문의</h4><h3>063-252-2988</h3>평일 09:00 - 18:00 (토/일/공휴일 휴무)
+        </div>
+    </section>
+    </div>
+)
+
+export default Footer;
